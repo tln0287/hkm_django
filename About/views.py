cbdc5814c8f6f56a87b851947999607120541214
imposter <lteku@gitam.edu>
2022-01-03 20:44:01 +0530

    nick jan 3

--- About/views.py
+++ About/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import AboutUs
 # Create your views here.
 
 def about(request):
-    return render(request,'about.html')
+    about = AboutUs.objects.all().values()
+    context = dict()
+    context['title'] = about[0]['title']
+    context['content'] = about[0]['content']
+    return render(request,'about.html', context)
