27d0748b9db8673737d0cd47e473f9488db175c6
lteku <lteku@gitam.edu>
2022-01-04 12:57:27 +0530

    Jan 4th

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from About.models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    slider = Slides.objects.all().values()
+    about = AboutUs.objects.all().values()
+
+    context = dict()
+    context['slider'] = slider
+    context['about'] = about
+    return render(request,'index.html',context)
