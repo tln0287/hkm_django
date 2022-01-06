9a482ed7bc3008a42f717e176b2dcfff598ecf4e
imposter <lteku@gitam.edu>
2022-01-06 10:22:41 +0530

    nick jan 6

--- Seva/admin.py
+++ Seva/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Sevas
-# Register your models here.
-admin.site.register(Sevas)
+from .models import *
+
+admin.site.register(VastrabaranaSeva)
+admin.site.register(PushpalankaraSeva)
+admin.site.register(AnnadanaSeva)
+admin.site.register(AbhishekaSeva)
+admin.site.register(NaivedyaSeva)
--- Seva/models.py
+++ Seva/models.py
@@ -1,12 +1,52 @@
 from django.db import models
 from tinymce.models import HTMLField
 from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
-class Sevas(models.Model):
+
+
+# Create your models here.
+class VastrabaranaSeva(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True)
     title = models.CharField(max_length=500, blank=True, null=True)
-    seva_image = models.ImageField(upload_to='sevas/')
     content = HTMLField(default="<p>Add content</p>")
+    image = models.ImageField(upload_to='vastrabaranaseva/',null=True)
 
     class Meta:
-        verbose_name_plural = "Sevas"
+        verbose_name_plural = "VastrabaranaSeva"
+
+class PushpalankaraSeva(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    content = HTMLField(default="<p>Add content</p>")
+    image = models.ImageField(upload_to='pushpalankaraseva/',null=True)
+
+    class Meta:
+        verbose_name_plural = "PushpalankaraSeva"
+
+class AnnadanaSeva(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    content = HTMLField(default="<p>Add content</p>")
+    image = models.ImageField(upload_to='annadanaseva/',null=True)
+
+    class Meta:
+        verbose_name_plural = "AnnadanaSeva"
+
+class AbhishekaSeva(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    content = HTMLField(default="<p>Add content</p>")
+    image = models.ImageField(upload_to='abhishekaseva/',null=True)
+
+    class Meta:
+        verbose_name_plural = "AbhishekaSeva"
+
+class NaivedyaSeva(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    content = HTMLField(default="<p>Add content</p>")
+    image = models.ImageField(upload_to='naivedyaseva/',null=True)
+
+    class Meta:
+        verbose_name_plural = "NaivedyaSeva"
