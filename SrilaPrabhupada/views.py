00ec17f230345dafd946baeeedf2fe067a361d1e
tln <teku.lakshminarayana@gmail.com>
2022-01-06 22:22:54 +0530

    Jan 6th

--- SrilaPrabhupada/views.py
+++ SrilaPrabhupada/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def archana(request):
+    data = Archana.objects.all().values()
+    context = dict()
+    context['data'] = data
 
-    return render(request, 'archana.html')
+    return render(request, 'archana.html',context)
 
 def biography(request):
-    return render(request, 'biography.html')
+    data = Biography.objects.all().values()
+    context = dict()
+    context['data'] = data
+    return render(request, 'biography.html',context)
 
 def books(request):
-    return render(request, 'books.html')
+    data = Books.objects.all().values()
+    context = dict()
+    context['data'] = data
+    return render(request, 'books.html',context)
 
 def lectures(request):
     return render(request, 'lectures.html')
